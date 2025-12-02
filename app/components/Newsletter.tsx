5e29fd05c46918b5134395a925b4d774434b87f5
Aylin Saber <saberaylin@gmail.com>
2025-12-02 09:59:17 +0100

    added newsletter

---
+++ app/components/Newsletter.tsx
@@ -0,0 +1,12 @@
+const Newsletter = () => {
+  return (
+    <div>
+      <h4 className='text-white'>Want the latest night club news</h4>
+      <p className='text-white'>
+        Subscribe to our newsletter and never miss an <span className='text-secondary'>Event</span>
+      </p>
+    </div>
+  );
+};
+
+export default Newsletter;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import WelcomeInNightclub from "./components/welcome-in-nightclub/WelcomeInNightclub";
+import Newsletter from "./components/Newsletter";
 
 export default function Home() {
-	return (
-		<div className="bg-black">
-			<main>
-				<WelcomeInNightclub />
-			</main>
-		</div>
-	);
+  return (
+    <div className='bg-black'>
+      <main>
+        <WelcomeInNightclub />
+        <Newsletter />
+      </main>
+    </div>
+  );
 }
