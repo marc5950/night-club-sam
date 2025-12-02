1f9cbc69ec2f314f786dcc9d3a75b667cf549fca
Sarah Timann <saol0003@stud.kea.dk>
2025-12-02 09:05:41 +0100

    add FormComment component with form handling using react-hook-form

--- app/(routes)/blog/[slug]/components/FormComment.tsx
+++ app/(routes)/blog/[slug]/components/FormComment.tsx
@@ -1,5 +1,24 @@
+"use client";
+import { useForm } from "react-hook-form";
+
 const FormComment = () => {
-  return <div>Form Component</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ defaultValues: { name: "", email: "", comment: "" } });
+
+  const onSubmit = (data: { name: string; email: string; comment: string }) => {
+    console.log("Form data:", data);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>Form Component</div>
+    </form>
+  );
 };
 
 export default FormComment;
