a92433906ba276aea8a0c06d011d2a2e031ffdba
Aylin Saber <saberaylin@gmail.com>
2025-12-03 21:40:52 +0100

    fixed hero small

--- app/components/general/HeroSmall.tsx
+++ app/components/general/HeroSmall.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 const HeroSmall = ({ title }: { title: string }) => {
   return (
-    <section className='h-[272px] w-full relative'>
+    <section className='relative w-full h-20 md:h-[272px]'>
       <Image
         src='/bg/footerbg.jpg'
         alt='Hero Background. Animated people with hands up.'
         width={1440}
         height={272}
-        className='object-cover w-full h-[272px] brightness-15'
+        className='object-cover w-full h-auto md:h-[272px] brightness-15'
       />
-      <div className='absolute inset-0 flex flex-col items-center justify-center'>
+      <div className='absolute inset-0 flex flex-col items-center justify-center z-1 mt-15 md:mt-0'>
         <h1 className='text-white text-4xl uppercase'>{title}</h1>
         <Image src='/bottom_line2.png' alt='Title underline pink' width={400} height={10} className='mt-2' />
       </div>
