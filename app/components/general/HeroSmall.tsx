aa91d972bc6d67d2435591ec8a7801d0be5d75e7
Aylin Saber <saberaylin@gmail.com>
2025-12-04 10:26:37 +0100

    fixed hero small

--- app/components/general/HeroSmall.tsx
+++ app/components/general/HeroSmall.tsx
@@ -8,9 +8,9 @@ const HeroSmall = ({ title }: { title: string }) => {
         alt='Hero Background. Animated people with hands up.'
         width={1440}
         height={272}
-        className='object-cover w-full h-auto md:h-[272px] brightness-15'
+        className='object-cover w-full md:h-[272px] brightness-15'
       />
-      <div className='absolute inset-0 flex flex-col items-center justify-center z-1 mt-15 md:mt-0'>
+      <div className='absolute inset-0 flex flex-col items-center justify-center z-1'>
         <h1 className='text-white text-4xl uppercase'>{title}</h1>
         <Image src='/bottom_line2.png' alt='Title underline pink' width={400} height={10} className='mt-2' />
       </div>
