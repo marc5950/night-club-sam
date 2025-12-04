429478abab3fdb1cd977b5acad01d83eece30943
Sarah Timann <saol0003@stud.kea.dk>
2025-12-04 10:18:09 +0100

    add BlogPost component and update blog post page to display post details

---
+++ app/(routes)/blog/[slug]/components/BlogPost.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { BlogPost as BlogPostType } from "@/app/types/api";
+
+interface BlogPostProps {
+  post: BlogPostType;
+}
+
+const BlogPost = ({ post }: BlogPostProps) => {
+  // Hent det specifikke blog post baseret på slug/id
+
+  const commentCount = post.comments?.length || 0;
+
+  return (
+    <div className="p-4 mt-24 max-w-[1440px]">
+      <div className="relative h-[400px] w-full mb-4">
+        <Image src={post.asset.url} alt={post.asset.alt || post.title} fill className="object-cover" unoptimized />
+      </div>
+      <h4 className="text-2xl mb-2">{post.title}</h4>
+      <p className="text-[#FF2A70] font-bold mb-2">
+        {post.author} / {commentCount} Comments / 16 Nov 2018
+      </p>
+      <p className="text-lg">{post.content}</p>
+    </div>
+  );
+};
+
+export default BlogPost;
--- app/(routes)/blog/[slug]/page.tsx
+++ app/(routes)/blog/[slug]/page.tsx
@@ -1,12 +1,36 @@
 import FormComment from "./components/FormComment";
+import BlogPost from "./components/BlogPost";
+import HeroSmall from "@/app/components/general/HeroSmall";
 
-const blogPost = () => {
+import { getBlogPosts, getBlogPostWithComments } from "@/app/lib/api";
+
+interface PageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+const blogPostSingleView = async ({ params }: PageProps) => {
+  const { slug } = await params;
+  const allPosts = await getBlogPosts();
+  const post = allPosts.find((p) => p.id.toString() === slug);
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  const postWithComments = await getBlogPostWithComments(post.id);
+  if (!postWithComments) {
+    return <div>Post with comments not found</div>;
+  }
+
   return (
     <main>
-      <h1>Blog Post</h1>
+      <HeroSmall title="Blog Post" />
+      {/* <h1>Blog Post</h1> */}
+      <BlogPost post={postWithComments} />
       <FormComment />
     </main>
   );
 };
 
-export default blogPost;
+export default blogPostSingleView;
