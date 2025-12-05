75dd4a131841c634a788fe8873638398ff437028
Aylin Saber <saberaylin@gmail.com>
2025-12-05 01:14:02 +0100

    added link around blog example

--- app/components/BlogExample.tsx
+++ app/components/BlogExample.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { BlogPost } from "@/app/types/api";
+import Link from "next/link";
 
 interface BlogCardProps {
   post: BlogPost;
@@ -20,17 +21,20 @@ const BlogExample = ({ post }: BlogCardProps) => {
     : "";
 
   return (
-    <article className='cursor-pointer'>
-      {/* Blog billede */}
-      <Image src={post.asset.url} alt={post.asset.alt || post.title || ""} fill className='object-cover' unoptimized />
-      <h2 className='text-background text-title3 font-normal uppercase'>{post.title}</h2>
-      {/* Meta information: forfatter, antal kommentarer, dato */}
-      <p className='text-secondary mt-2 mb-6 text-p-big font-normal'>
-        By: {post.author} / {commentCount} {commentCount === 1 ? "kommentar" : "kommentarer"} / {formattedDate}
-      </p>
-      {/* Kort preview af indholdet (første 20 ord). Tilføjer ... til sidst */}
-      <p className='text-background'>{post.content.split(" ").slice(0, 20).join(" ")}...</p>
-    </article>
+    // Link til det fulde blogindlæg
+    <Link href={`/blog/${post.slug || post.id}`}>
+      <article className='cursor-pointer'>
+        {/* Blog billede */}
+        <Image src={post.asset.url} alt={post.asset.alt || post.title || ""} fill className='object-cover' unoptimized />
+        <h2 className='text-background text-title3 font-normal uppercase'>{post.title}</h2>
+        {/* Meta information: forfatter, antal kommentarer, dato */}
+        <p className='text-secondary mt-2 mb-6 text-p-big font-normal'>
+          By: {post.author} / {commentCount} {commentCount === 1 ? "kommentar" : "kommentarer"} / {formattedDate}
+        </p>
+        {/* Kort preview af indholdet (første 20 ord). Tilføjer ... til sidst */}
+        <p className='text-background'>{post.content.split(" ").slice(0, 20).join(" ")}...</p>
+      </article>
+    </Link>
   );
 };
 
