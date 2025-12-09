e18014ec6e3ebd7866cc8ad3c810a222a281d453
Sarah Timann <saol0003@stud.kea.dk>
2025-12-09 17:47:17 +0100

    tilføjet Gallery indhold til MusicTrack

--- app/components/MusicTrack.tsx
+++ app/components/MusicTrack.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 import { FaRegCirclePlay } from "react-icons/fa6";
 import { FaRegCirclePause } from "react-icons/fa6";
@@ -7,13 +8,17 @@ import { FaFastForward } from "react-icons/fa";
 import { FaBackwardFast } from "react-icons/fa6";
 import { FaShuffle } from "react-icons/fa6";
 import { FaVolumeUp } from "react-icons/fa";
+import { getGalleryPhotos } from "@/app/lib/api";
+import { GalleryPhoto } from "@/app/types/api";
 
-const MusicTrack = () => {
+const MusicTrack = async () => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
   const [progress, setProgress] = useState(0);
   const [duration, setDuration] = useState(0);
+
+  const photos: GalleryPhoto[] = await getGalleryPhotos();
 
   const tracks = [
     { title: "BLACK BOX FUNKY", src: "/media/black-box-funky.mp3" },
@@ -68,69 +73,72 @@ const MusicTrack = () => {
   };
 
   return (
-    <div className="max-w-[500px] mx-auto p-4 flex flex-col items-center gap-4">
-      <audio className="" ref={audioRef} src={tracks[currentTrackIndex].src} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleDurationChange} onEnded={skipForward} />
+    <div className="max-w-[1440px]">
+        
+      <div className="max-w-[500px] mx-auto p-4 flex flex-col items-center gap-4">
+        <audio className="" ref={audioRef} src={tracks[currentTrackIndex].src} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleDurationChange} onEnded={skipForward} />
 
-      {/* Titel */}
-      <div className="w-full flex justify-start">
-        <p className="font-medium text-primary">{tracks[currentTrackIndex].title}</p>
-      </div>
+        {/* Titel */}
+        <div className="w-full flex justify-start">
+          <p className="font-medium text-primary">{tracks[currentTrackIndex].title}</p>
+        </div>
 
-      {/* Fremdriftslinje */}
-      <div className="flex items-center gap-2 w-full">
-        <span>{formatTime(audioRef.current?.currentTime || 0)}</span>
-        <input
-          type="range"
-          className="flex-1 h-1 custom-range"
-          max={100}
-          value={isNaN(progress) ? 0 : progress}
-          onChange={(e) => {
-            if (!audioRef.current) return;
-            const newTime = (Number(e.target.value) / 100) * audioRef.current.duration;
-            audioRef.current.currentTime = newTime;
-            setProgress(Number(e.target.value));
-          }}
-        />
-        <span>{formatTime(duration)}</span>
-      </div>
-
-      {/* Knapper + volume */}
-      <div className="flex items-center gap-4 mt-2 w-full">
-        {/* Frem / Tilbage */}
-        <button onClick={skipBackward} className="text-primary text-2xl">
-          <FaBackwardFast />
-        </button>
-
-        {/* Play / Pause */}
-        <button onClick={togglePlay} className="text-primary text-4xl">
-          {isPlaying ? <FaRegCirclePause /> : <FaRegCirclePlay />}
-        </button>
-
-        {/* Frem / Fremad */}
-        <button onClick={skipForward} className="text-primary text-2xl">
-          <FaFastForward />
-        </button>
-
-        {/* Shuffle */}
-        <button onClick={shuffleTrack} className="text-primary text-2xl">
-          <FaShuffle />
-        </button>
-
-        {/* Volume */}
-        <div className="flex items-center gap-2">
-          <FaVolumeUp className="text-primary text-2xl" />
+        {/* Fremdriftslinje */}
+        <div className="flex items-center gap-2 w-full">
+          <span>{formatTime(audioRef.current?.currentTime || 0)}</span>
           <input
             type="range"
-            className="flex-1 h-1 custom-range w-18"
-            min={0}
-            max={1}
-            step={0.01}
-            value={audioRef.current?.volume || 0.5}
+            className="flex-1 h-1 custom-range"
+            max={100}
+            value={isNaN(progress) ? 0 : progress}
             onChange={(e) => {
               if (!audioRef.current) return;
-              audioRef.current.volume = Number(e.target.value);
+              const newTime = (Number(e.target.value) / 100) * audioRef.current.duration;
+              audioRef.current.currentTime = newTime;
+              setProgress(Number(e.target.value));
             }}
           />
+          <span>{formatTime(duration)}</span>
+        </div>
+
+        {/* Knapper + volume */}
+        <div className="flex items-center gap-4 mt-2 w-full">
+          {/* Frem / Tilbage */}
+          <button onClick={skipBackward} className="text-primary text-2xl">
+            <FaBackwardFast />
+          </button>
+
+          {/* Play / Pause */}
+          <button onClick={togglePlay} className="text-primary text-4xl">
+            {isPlaying ? <FaRegCirclePause /> : <FaRegCirclePlay />}
+          </button>
+
+          {/* Frem / Fremad */}
+          <button onClick={skipForward} className="text-primary text-2xl">
+            <FaFastForward />
+          </button>
+
+          {/* Shuffle */}
+          <button onClick={shuffleTrack} className="text-primary text-2xl">
+            <FaShuffle />
+          </button>
+
+          {/* Volume */}
+          <div className="flex items-center gap-2">
+            <FaVolumeUp className="text-primary text-2xl" />
+            <input
+              type="range"
+              className="flex-1 h-1 custom-range w-18"
+              min={0}
+              max={1}
+              step={0.01}
+              value={audioRef.current?.volume || 0.5}
+              onChange={(e) => {
+                if (!audioRef.current) return;
+                audioRef.current.volume = Number(e.target.value);
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
