a6169a24fc921efa3d524a6b80c5ab614b5c13db
Aylin Saber <saberaylin@gmail.com>
2025-12-15 17:32:35 +0100

    added animation to events

--- app/components/events/EventCard.tsx
+++ app/components/events/EventCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { Event } from "@/app/types/api";
+import { motion } from "framer-motion";
 
 interface EventCardProps {
   event: Event;
@@ -21,14 +22,50 @@ const EventCard = ({ event }: EventCardProps) => {
   });
 
   return (
-    <article className='relative mb-9'>
-      <Image src={event.asset.url} alt={event.title} width={600} height={400} />
-      <div className='flex absolute bottom-0 left-0  bg-secondary text-primary w-full gap-4 p-1 text-[20px]'>
+    <motion.article
+      className='relative mb-15'
+      whileHover='hover' // Hover tilstand
+      initial='rest' // Starttilstand
+      animate='rest' // Normal tilstand
+    >
+      <div className='relative'>
+        <Image src={event.asset.url} alt={event.title} width={600} height={400} />
+        <motion.div
+          className='absolute inset-0 bg-black'
+          variants={{
+            rest: { opacity: 0 },
+            hover: { opacity: 0.5 },
+          }}
+          transition={{ duration: 0.1 }}
+        />
+        <motion.button
+          className='absolute left-1/2 -translate-x-1/2 top-25 bg-secondary text-white p-2'
+          variants={{
+            rest: { opacity: 0, y: -30 },
+            hover: { opacity: 1, y: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+        >
+          Book Now
+        </motion.button>
+        <motion.div
+          className='absolute left-0 bottom-0 bg-black p-3  flex flex-col items-start gap-1 text-primary'
+          variants={{
+            rest: { opacity: 0, y: 20 },
+            hover: { opacity: 1, y: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+        >
+          <div className='text-primary text-title3'>{event.title}</div>
+          <div className='text-primary text-p-small line-clamp-3'>{event.description}</div>
+        </motion.div>
+      </div>
+      <div className='flex absolute bg-secondary text-primary w-full gap-4 p-1 text-p-big'>
         <span>{formattedDate}</span>
         <span>{formattedTime}</span>
         <span className='ml-3'>{event.location}</span>
       </div>
-    </article>
+    </motion.article>
   );
 };
 
