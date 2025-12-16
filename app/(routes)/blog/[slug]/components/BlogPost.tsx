7c48b3b2079a743b38baeb4e2c9459cfc0ec9c31
Sarah Timann <saol0003@stud.kea.dk>
2025-12-16 12:25:04 +0100

    fixed date display in BlogPost component

--- app/(routes)/blog/[slug]/components/BlogPost.tsx
+++ app/(routes)/blog/[slug]/components/BlogPost.tsx
@@ -9,6 +9,13 @@ const BlogPost = ({ post }: BlogPostProps) => {
   // Hent det specifikke blog post baseret på slug/id
 
   const commentCount = post.comments?.length || 0;
+  const formattedDate = post.date
+    ? new Date(post.date).toLocaleDateString("da-DK", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      })
+    : "";
 
   return (
     <article className="mt-14 max-w-[1440px] md:ml-42 md:mr-42">
@@ -16,9 +23,9 @@ const BlogPost = ({ post }: BlogPostProps) => {
         <Image src={post.asset.url} alt={post.asset.alt || post.title} fill className="object-cover" unoptimized />
       </div>
       <div className="p-4 md:p-0">
-        <h4 className="text-2xl mb-2 text-primary">{post.title}</h4>
-        <p className="text-secondary font-bold mb-2">
-          {post.author} / {commentCount} Comments / 16 Nov 2018
+        <h4 className="text-2xl mb-2 uppercase text-primary">{post.title}</h4>
+        <p className="text-secondary mb-2">
+          {post.author} / {commentCount} Comments / {formattedDate}
         </p>
         <p className="text-lg text-primary">{post.content}</p>
       </div>
