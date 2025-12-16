51e7940c0907030a8f8968f200f46593878346a6
Marcus Wagner <marcuswagner5950@gmail.com>
2025-12-16 13:27:43 +0100

    fixed no useeffect til fetch

--- app/components/testimonials/TestimonialList.tsx
+++ app/components/testimonials/TestimonialList.tsx
@@ -1,41 +1,31 @@
 "use client";
-import { useState, useEffect } from "react";
-import { getTestimonials } from "@/app/lib/api";
+import { useState } from "react";
 import { Testimonial } from "@/app/types/api"; // typescript interface for testimonial
 import TestimonialCard from "./TestimonialCard";
 import ScrollBar from "../ScrollBar";
 
-const TestimonialList = () => {
-  // testimonialData: Array af testimonials
-  // setTestimonialData: Funktion der opdaterer testimonialData
-  // start med tom array og index 0
-  const [testimonialData, setTestimonialData] = useState<Testimonial[]>([]);
-  // activeIndex holder styr på hvilken testimonial der vises. Starter på 0 (den første testimonial)
-  const [activeIndex, setActiveIndex] = useState(0);
+type Props = { items: Testimonial[] };
 
-  useEffect(() => {
-    // Kører API'et for at hente testimonials når komponenten er loadet
-    const fetchData = async () => {
-      const data = await getTestimonials();
-      // Sætter den hentede data i state variablen testimonialData
-      setTestimonialData(data);
-    };
-    // Kalder fetchData funktionen
-    fetchData();
-  }, []);
+const TestimonialList = ({ items }: Props) => {
+	// testimonialData: Array af testimonials
+	// setTestimonialData: Funktion der opdaterer testimonialData
+	// start med tom array og index 0
+	const testimonialData = items;
+	// activeIndex holder styr på hvilken testimonial der vises. Starter på 0 (den første testimonial)
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  // Hvis der ikke er nogen testimonials, returner null (vis ingenting)
-  if (!testimonialData[activeIndex]) return null;
+	// Hvis der ikke er nogen testimonials, returner null (vis ingenting)
+	if (!testimonialData[activeIndex]) return null;
 
-  return (
-    <section className='max-w-6xl mx-auto p-8 mt-20 mb-20'>
-      {/* Viser den aktive testimonial baseret på activeIndex */}
-      {/* hvis activeIndex = 0, så vis testimonialData[0] */}
-      <TestimonialCard post={testimonialData[activeIndex]} />
-      {/* ScrollBar komponent til at skifte mellem testimonials */}
-      <ScrollBar active={activeIndex} onSelect={setActiveIndex} activeColor='bg-secondary' color='bg-white' />
-    </section>
-  );
+	return (
+		<section className="max-w-6xl mx-auto p-8 mt-20 mb-20">
+			{/* Viser den aktive testimonial baseret på activeIndex */}
+			{/* hvis activeIndex = 0, så vis testimonialData[0] */}
+			<TestimonialCard post={testimonialData[activeIndex]} />
+			{/* ScrollBar komponent til at skifte mellem testimonials */}
+			<ScrollBar active={activeIndex} onSelect={setActiveIndex} activeColor="bg-secondary" color="bg-white" />
+		</section>
+	);
 };
 
 export default TestimonialList;
